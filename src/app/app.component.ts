43e1478e5bff14e5fb57c9132f6c877e5ebc284a
Punya <punya.sheel@gmail.com>
2019-10-27 17:30:22 +0530

    Updated changes - 85

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,11 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  
+  loadedOption = 'recipe';
+
+  onNavigate(optionSelected: string) {
+    this.loadedOption = optionSelected;
+  }
 }
