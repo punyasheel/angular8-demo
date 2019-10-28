795b8d6232ed2bdbff53dadaf6b442f87f0b9deb
Punya <punya.sheel@gmail.com>
2019-10-28 16:54:42 +0530

    103

---
+++ src/app/shared/dropdown.drective.ts
@@ -0,0 +1,27 @@
+/*
+import { Directive, HostListener, HostBinding } from '@angular/core';
+
+@Directive({
+    selector: '[appDropdown]'
+})
+export class DropdownDirective {
+    @HostBinding('class.open') isOpen = false;
+
+    @HostListener('click') toggleOpen() {
+        this.isOpen = !this.isOpen;
+    }
+}
+*/
+/* code below to close dropdown when mouse clicked anywhere else other than just the dropdown */
+import {Directive, ElementRef, HostBinding, HostListener} from '@angular/core';
+ 
+@Directive({
+  selector: '[appDropdown]'
+})
+export class DropdownDirective {
+  @HostBinding('class.open') isOpen = false;
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
+  }
+  constructor(private elRef: ElementRef) {}
+}
