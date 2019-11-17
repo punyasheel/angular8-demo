5f7278d099d3e979c29e5c1c04ae49368c1299f2
Punya <punya.sheel@gmail.com>
2019-11-17 14:17:36 +0530

    170

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,4 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-  loadedOption = 'recipe';
-
-  onNavigate(optionSelected: string) {
-    this.loadedOption = optionSelected;
-  }
 }
